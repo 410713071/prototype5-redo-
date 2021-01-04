06dc8636c5a26a3d9594420114d907768cb809dd
410713071 <410713071@gms.ndhu.edu.tw>
2021-01-05 01:06:15 +0800

    無限或重複的使GameManager產生物件

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private float spawnRate = 1.0f;
     public List<GameObject> targets;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnTarget());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
     }
 }
