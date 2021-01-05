fe93751ca3b22e197d6076a4271a45f3bc6cefe0
410713071 <410713071@gms.ndhu.edu.tw>
2021-01-06 01:08:24 +0800

    給予每個物件一個分數

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -11,6 +11,7 @@ public class Target : MonoBehaviour
     private float xRange = 4;
     private float ySpawnPos = -6;
     private GameManager gameManager;
+    public int pointValue;
     // Start is called before the first frame update
     void Start()
     {
@@ -49,7 +50,7 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
-        gameManager.UpdateScore(5);
+        gameManager.UpdateScore(pointValue);
     }
     private void OnTriggerEnter(Collider other)
     {
