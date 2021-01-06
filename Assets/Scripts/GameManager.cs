04eb8953552cae61b45b3a8704fe19fb9b9042bd
410713071 <410713071@gms.ndhu.edu.tw>
2021-01-06 13:23:47 +0800

    觸發遊戲結束

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,12 +9,15 @@ public class GameManager : MonoBehaviour
     public List<GameObject> targets;
     private int score;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
+        gameOverText.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
